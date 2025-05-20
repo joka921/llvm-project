02822968d515627c70a9e0c66e09884aa364b283
Johannes Kalmbach <johannes.kalmbach@gmail.com>
2025-05-20 18:07:12 +0200

    We have correctly identified generators... yay:)

--- clang-tools-extra/backport-defaulted-equality/BackportDefaultedEquality.cpp
+++ clang-tools-extra/backport-defaulted-equality/BackportDefaultedEquality.cpp
@@ -7,6 +7,7 @@
 #include "clang/Tooling/CommonOptionsParser.h"
 #include "clang/Tooling/Tooling.h"
 #include "clang/ASTMatchers/ASTMatchers.h"
+#include "clang/AST/RecursiveASTVisitor.h"
 #include "clang/ASTMatchers/ASTMatchFinder.h"
 #include "clang/Rewrite/Core/Rewriter.h"
 
@@ -44,6 +45,193 @@ static DeclarationMatcher ClassMatcher = cxxRecordDecl(
         bind(
             "record"
         );
+
+/*
+static DeclarationMatcher GeneratorMatcher = functionDecl(
+
+        hasBody(compoundStmt(
+                forEachDescendant(coyieldExpr())
+        ))).bind("function");
+        */
+static DeclarationMatcher GeneratorMatcher = functionDecl(isDefinition(), hasBody(hasDescendant(stmt(coyieldExpr()).bind("coYield"))))
+        .bind("function");
+/*
+static DeclarationMatcher GeneratorMatcher = functionDecl(isDefinition(), hasBody(compoundStmt(hasDescendant(coyieldExpr())))
+        ).bind("function");
+        */
+
+void collectVarDeclsFromStmt(Stmt* stmt, std::vector<VarDecl*>& outVarDecls) {
+    using namespace clang;
+    if (!stmt) return;
+
+    class VarDeclCollector : public RecursiveASTVisitor<VarDeclCollector> {
+    public:
+        std::vector<VarDecl*>& Collected;
+
+        VarDeclCollector(std::vector<VarDecl*>& collected)
+            : Collected(collected) {}
+
+        bool TraverseDecl(Decl* D) {
+            //outs() << "traversing a decl at " << D << '\n';
+            if (! D) { return true;}
+            // Skip traversing into class declarations (local classes)
+            if (isa<CXXRecordDecl>(D) && cast<CXXRecordDecl>(D)->isLocalClass()) {
+                return true;  // Do not recurse into the class
+            }
+
+            // Skip lambdas
+            if (isa<CXXMethodDecl>(D) &&
+                cast<CXXMethodDecl>(D)->getParent()->isLambda()) {
+                return true;  // Don't recurse into lambda method
+            }
+
+            return RecursiveASTVisitor::TraverseDecl(D);
+        }
+
+        bool TraverseLambdaExpr(LambdaExpr* LE) {
+            return true; // Don't recurse into the lambda body
+        }
+
+        bool VisitDeclStmt(DeclStmt* declStmt) {
+            //outs() << "taversing a declStmt at " << declStmt << '\n';
+            for (Decl* decl : declStmt->decls()) {
+                if (auto* varDecl = dyn_cast<VarDecl>(decl)) {
+                    Collected.push_back(varDecl);
+                }
+            }
+            return true;
+        }
+    };
+
+    VarDeclCollector collector(outVarDecls);
+    collector.TraverseStmt(stmt);
+}
+
+// This class first collects all the matching generator and then via its `rewrite` method rewrites them.
+class GeneratorRewriter : public MatchFinder::MatchCallback {
+    // Class for a single matching class
+    /*
+    struct Res {
+        // The position of the defaulted operator, needed to later replace it.
+        SourceRange operatorPosition;
+        // The name of the class (without enclosing namespaces)
+        std::string className;
+        // The names of all non-static members of the class.
+        std::vector<std::string> fieldNames;
+        // The fully-qualified (including base-classes)
+        std::vector<std::string> baseClassNames;
+        bool hasError = false;
+    };
+
+    std::vector<Res> matches;
+     */
+
+    Rewriter &rewriter;
+    const SourceManager &sourceManager;
+    clang::DiagnosticsEngine &diagnosticsEngine;
+
+public :
+    GeneratorRewriter(Rewriter &rewr, const SourceManager &manager, DiagnosticsEngine &engine) : rewriter(rewr),
+                                                                                                        sourceManager(manager), diagnosticsEngine(engine) {
+    }
+
+    struct localVarPositions {
+     std::string type;
+     std::string name;
+     SourceRange pos;
+   };
+
+    struct Res {
+        SourceRange coroutinePosition;
+        std::vector<localVarPositions> decls;
+    };
+    std::vector<Res> coroutines;
+
+    // This method is called for each class that matches the matcher defined above.
+    void run(const MatchFinder::MatchResult &Result) override {
+        const auto *Decl = Result.Nodes.getNodeAs<clang::FunctionDecl>("function");
+        const auto *stmt = Result.Nodes.getNodeAs<Stmt>("coYield");
+
+        if (!Decl || !stmt) return;
+        auto fileId = sourceManager.getFileID(Decl->getLocation());
+        if (fileId != sourceManager.getMainFileID()) {
+            /*
+            outs() << "fileId doesn't match\n";
+            unsigned diagID = diagnosticsEngine.getCustomDiagID(clang::DiagnosticsEngine::Remark,
+                                                                "found a cppcoro::generator in wrong file.");
+            diagnosticsEngine.Report(sourceManager.getFileLoc(Decl->getSourceRange().getBegin()), diagID);
+             */
+            return;
+        }
+        //outs() << "found a generator\n";
+
+        ASTContext &Ctx = *Result.Context;
+
+        // Walk up the parent chain of the coroutine stmt
+        const Stmt *current = stmt;
+        while (current) {
+            const auto &parents = Ctx.getParents(*current);
+            if (parents.empty()) break;
+
+            const DynTypedNode &parentNode = parents[0];
+
+            if (const auto *parentDecl = parentNode.get<::Decl>()) {
+                if (const auto *record = dyn_cast<CXXRecordDecl>(parentDecl)) {
+                      outs() << "found local decl while traversing " << record << '\n';
+                    if (record->isLambda() || record->isLocalClass()) {
+                        return; // Coroutine stmt is nested inside a lambda or local class
+                    }
+                }
+            }
+
+            // Stop when we've reached the function itself
+            if (const ::Decl *D = parentNode.get<FunctionDecl>()) {
+                if (D == Decl)  {
+                    outs() << "break because we've found the parent" << '\n';
+                    break;  // OK, we’ve walked all the way to the target function
+                    }
+            }
+
+            if (const auto *parentStmt = parentNode.get<Stmt>()) {
+                current = parentStmt;
+            } else {
+                outs() << "break because we've found nothing..." << '\n';
+                return;
+            }
+        }
+
+
+        std::vector<VarDecl*> decls;
+        coroutines.emplace_back();
+        auto& c = coroutines.back();
+        coroutines.back().coroutinePosition = Decl->getSourceRange();
+
+        auto body = Decl->getBody();
+        if (! body) {
+            return;
+        }
+        //outs() << "collecting decls, body is " << body <<"\n" ;
+        collectVarDeclsFromStmt(Decl->getBody(), decls);
+        //outs() << "finished collecting decls\n";
+        for (auto* varDecl : decls) {
+            c.decls.push_back(localVarPositions{varDecl->getNameAsString(), varDecl->getType().getAsString(), varDecl->getSourceRange()});
+            auto&b = c.decls.back();
+            outs() << "found a member variable \"" << b.name << "\" with type \"" << b.type << "\"\n";
+        }
+        //outs() << "after printing decls\n";
+
+        // Don't rewrite classes that are contained in included header files, only rewrite the currently active file.
+        unsigned diagID = diagnosticsEngine.getCustomDiagID(clang::DiagnosticsEngine::Remark,
+                                                            "found a cppcoro::generator.");
+        diagnosticsEngine.Report(sourceManager.getFileLoc(Decl->getSourceRange().getBegin()), diagID);
+    }
+
+    // This function can be manually called once all the matching classes have been processed.
+    // It performs the actual rewrite.
+    void rewrite() {
+    return;
+    }
+};
 
 // This class first collects all the matching classes, and then via its `rewrite` method rewrites them.
 class DefaultOperatorsRewriter : public MatchFinder::MatchCallback {
@@ -120,11 +308,11 @@ public :
             std::string rewrite = "bool operator==(const " + className + "& " + nameOfOther + ") const {\n";
             for (const auto &baseClass: m.baseClassNames) {
                 rewrite += "    if (!(static_cast<const " + baseClass + "&>(*this) == static_cast<const " + baseClass +
-                        "&>(otherRhs))) return false;\n";
+                        "&>(otherRhs))) { return false;}\n";
             }
 
             for (const auto &mem: m.fieldNames) {
-                rewrite += "    if (!(" + mem + " == otherRhs." + mem + ")) return false;\n";
+                rewrite += "    if (!(" + mem + " == otherRhs." + mem + ")) {return false;}\n";
             }
             rewrite += "    return true;\n  }";
             rewriter.ReplaceText(m.operatorPosition, rewrite);
@@ -136,8 +324,9 @@ class MyASTConsumer : public ASTConsumer {
 public:
     using P = std::unique_ptr<Rewriter>;
 
-    MyASTConsumer(DefaultOperatorsRewriter &callback) : Callback(callback) {
-        Finder.addMatcher(ClassMatcher, &Callback);
+    MyASTConsumer(DefaultOperatorsRewriter &callback, GeneratorRewriter& generatorCallback) {
+        Finder.addMatcher(ClassMatcher, &callback);
+        Finder.addMatcher(GeneratorMatcher, &generatorCallback);
     }
 
     void HandleTranslationUnit(ASTContext &Context) override {
@@ -146,8 +335,8 @@ public:
     }
 
 private:
+
     //Rewriter& Rewrite;
-    DefaultOperatorsRewriter &Callback;
     MatchFinder Finder;
 };
 
@@ -156,6 +345,7 @@ class RewriteDefaultOperatorsFrontendAction : public ASTFrontendAction {
     std::unique_ptr<Rewriter> Rewrite = std::make_unique<Rewriter>();
     const SourceManager *SM;
     std::unique_ptr<DefaultOperatorsRewriter> defaultOperatorsRewriter;
+    std::unique_ptr<GeneratorRewriter> generatorRewriter;
 
 public:
     // This function sets up the `DefaultOperatorsRewriter`.
@@ -164,12 +354,13 @@ public:
         // Initialize the Rewriter with the SourceManager from the CompilerInstance
         SM = &CI.getSourceManager();
         defaultOperatorsRewriter = std::make_unique<DefaultOperatorsRewriter>(*Rewrite, *SM, CI.getDiagnostics());
+        generatorRewriter = std::make_unique<GeneratorRewriter>(*Rewrite, *SM, CI.getDiagnostics());
 
         // Set the rewriter's buffer to the file being processed
         Rewrite->setSourceMgr(CI.getSourceManager(), CI.getLangOpts());
 
         // Set up the callback for replacing text in the source code
-        return std::make_unique<MyASTConsumer>(*defaultOperatorsRewriter);
+        return std::make_unique<MyASTConsumer>(*defaultOperatorsRewriter, *generatorRewriter);
     }
 
     // TODO<joka921> The following was suggested by ChatGPT to modify the files in place.
@@ -180,6 +371,9 @@ public:
 
     // Save the modified source code back to the file
     void EndSourceFileAction() override {
+        // TODO<joka921> This early return has to be removed to fix the final
+        // thing.
+        return;
         defaultOperatorsRewriter->rewrite();
         // Write the modified content back to the original file
 
